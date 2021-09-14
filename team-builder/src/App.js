2ae4b1f68f626f37b6661d9dbc12585684a21db6
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-09-14 16:16:24 -0400

    initializing state

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,13 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
+const team = [
+  {
+    name:'Tom',
+    age: '34',
+    role: 'front-end',
+    tag: 'Carrots',
+    senior: true
+  },
+  {
+    name:'Lou',
+    age: '35',
+    role: 'back-end',
+    tag: 'anti-lou',
+    senior: true
+  },
+  {
+    name:'Ericka',
+    age: '26',
+    role: 'design',
+    tag: 'Chibi',
+    senior: false
+  },
+  {
+    name:'Alberto',
+    age: '32',
+    role: 'front-end',
+    tag: 'Lethargiry',
+    senior: true
+  },
+]
 function App() {
+  const [teamMembers, setTeamMemebrs] = useState()
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to relewd.
         </p>
         <a
           className="App-link"
