d58562efaf93003afb3f971b2b946b84f23d9b2b
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-09-14 16:33:06 -0400

    list renedered

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -2,54 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import React, {useState} from 'react';
 
-const team = [
-  {
-    name:'Tom',
-    age: '34',
-    role: 'front-end',
-    tag: 'Carrots',
-    senior: true
-  },
-  {
-    name:'Lou',
-    age: '35',
-    role: 'back-end',
-    tag: 'anti-lou',
-    senior: true
-  },
-  {
-    name:'Ericka',
-    age: '26',
-    role: 'design',
-    tag: 'Chibi',
-    senior: false
-  },
-  {
-    name:'Alberto',
-    age: '32',
-    role: 'front-end',
-    tag: 'Lethargiry',
-    senior: true
-  },
+//team values: name, age, role, tag, senior
+const initialTeam = [
+  {name:'Tom',age: '34',role: 'front-end',tag: 'Carrots',senior: true},
+  {name:'Lou',age: '35',role: 'back-end',tag: 'anti-lou',senior: true},
+  {name:'Ericka',age: '26',role: 'designer',tag: 'Chibi',senior: false},
+  {name:'Alberto',age: '32',role: 'tester',tag: 'Lethargiry',senior: true},
 ]
 function App() {
-  const [teamMembers, setTeamMemebrs] = useState()
+  const [teamMembers, setTeamMemebrs] = useState(initialTeam)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to relewd.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {teamMembers.map((tm,idx)=>{
+       return(
+       <h2 key={idx}>{tm.name} or {tm.tag}: our {tm.senior ? 'senior ':''}{tm.role}</h2>
+       )
+     })}
     </div>
   );
 }
